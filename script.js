aae2b9fac6ddf1f418928916443bb8b431a798b9
LN27100 <heloceinlove@hotmail.fr>
2023-10-04 16:37:11 +0200

    début js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const heroes = ['Batman', 'Superman', 'Catwoman']
+const heroes = [
+    ['Batman', 'batman.jpg', 'Super riche'],
+    ['Superman', 'superman.jpg', 'Super riche'],
+    ['Catwoman', 'catwoman.jpg', 'Super belle']
+]
+const container = document.getElementById('container')
 
 for (let i = 0; i <= 2; i++) {
-    console.log ()
+    container.innerHTML += `<h2 class="text-primary col-4">${heroes[i]}</h2>`
+    `<p $Batman</p>`
+    `<img src="batman.jpg" alt="Image de Batman">`
+    `<p>Super riche</p>`
 }
