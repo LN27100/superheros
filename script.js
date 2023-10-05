bbfac293acfd3be8471256e63b4598252f749dc1
LN27100 <heloceinlovehotmail.fr>
2023-10-05 11:46:29 +0200

    images

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const heroes = [
-    ['Batman', 'batman.jpg', 'Super riche'],
-    ['Superman', 'superman.jpg', 'Super riche'],
-    ['Catwoman', 'catwoman.jpg', 'Super belle']
+    ['Wonderwoman', 'wonderwoman.jpg', 'Super sexy'],
+    ['Hulk', 'hulk.jpg', 'Super fort'],
+    ['Black Widow', 'blackwidow.jpg', 'Super classe'],
+    ['Deadpool', 'deadpool.jpg', 'Super fun'],
+    ['Wolverine', 'wolverine.jpg', 'Super tranchant'],
+    ['Captain Marvel', 'captainmarvel.jpg', 'Super autoritaire'],
+    ['Spiderman', 'spiderman.jpg', 'Super aggripant'],
+    ['Phoenix', 'phoenix.jpg', 'Super brûlante'],
+    ['Flash', 'flash.jpg', 'Super rapide'],
 ]
 const container = document.getElementById('container')
 
-for (let i = 0; i <= 2; i++) {
-    container.innerHTML += `<h2 class="text-primary col-4">${heroes[i]}</h2>`
-    `<p $Batman</p>`
-    `<img src="batman.jpg" alt="Image de Batman">`
-    `<p>Super riche</p>`
-}
+    
+    for (let i = 0; i<= 9; i++){
+        console.log(heroes[i])
+        container.innerHTML+= ` <div class ='col-4 border rounded shadow'>
+        <p>${heroes[i][0]}</p>
+        <img src= ${heroes[i][1]} alt="image de ${heroes[i][0]}">
+        <p>${heroes[i][2]}</p>
+       </div>`
+    }
+    
+     
+    
+     
